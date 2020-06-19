2f43931e753a93d4569505c15ac6d0e5b443e6f4
Shereen789 <irfanashereen@msitprogram.net>
2020-06-19 19:46:22 +0530

    Jun 19  19:46:22

--- tests/test_add.py
+++ tests/test_add.py
@@ -4,7 +4,7 @@ from solution.solution import add
 import pytest
 
 @pytest.mark.parametrize('x,y,result',[
-    (10,10,20),(5,5,11),(6,6,12)
+    (10,10,20),(5,5,10),(6,6,12)
 ])
 def test_add(x,y,result):
     assert add(x,y) == result
